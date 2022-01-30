e37f6c0876f91a8bcd57998015ed555078dcb107
Olarewaju Mojeed <53194190+Lowkey1729@users.noreply.github.com>
2022-01-30 18:30:32 +0100

    Included The Constant class

---
+++ src/Classes/Constants.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Description of Constants
+ *
+ */
+
+namespace App\Classes;
+
+class Constants {
+
+    const SANDBOX_BASE_URL = "";
+    const PRODUCTION_BASE_URL = "";
+    const DEV_BASE_URL = "";
+    const PASSPORT_RESOURCE_URL = "";
+    const HTTP_CODE = "HTTP_CODE";
+    const RESPONSE_BODY = "RESPONSE_BODY";
+    const TIMESTAMP = "TIMESTAMP";
+    const NONCE = "NONCE";
+    const SIGNATURE_METHOD = "SIGNATURE_METHOD";
+    const SIGNATURE = "SIGNATURE";
+    const AUTHORIZATION = "AUTHORIZATION";
+    const CONTENT_TYPE = "application/json";
+    const SIGNATURE_METHOD_VALUE = "SHA1";
+    const LAGOS_TIME_ZONE = "Africa/Lagos";
+    const BEARER_AUTHORIZATION_REALM = "Bearer";
+
+
+
+}
+
