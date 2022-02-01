3a49cdee0add65a4ce681bb813ec75f0a907ce8d
Olarewaju Mojeed <53194190+Lowkey1729@users.noreply.github.com>
2022-02-01 01:27:01 +0100

    Fixed the merge conflicts

--- src/Contracts/Nigeria/IdentityPassContract.php
+++ src/Contracts/Nigeria/IdentityPassContract.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace IdentityPass\IdentityPass\Contracts\Nigeria;
 
+namespace IdentityPass\IdentityPass\Contracts\Nigeria;
 /**
  * @param null $path url path
  *
@@ -10,73 +10,103 @@ namespace IdentityPass\IdentityPass\Contracts\Nigeria;
 
 interface IdentityPassContract
 {
+
     /**
      * @param null $path url path
      *
      * @return string
      */
-    public static function url($path = null): string;
+
+    static function url( $path = null ): string;
+
 
     public static function headers(): array;
 
+
     public static function getSecretKey(): string;
 
-<<<<<<< HEAD
-    public static function plateNumberVerification(): array;
-=======
 
     public static function plateNumberVerification($vehicleNumber): array;
->>>>>>> e55e055 (api integration)
+
 
     public static function bvnWithFaceVerification(): array;
 
+
     public static function bvn2Verification(): array;
+
 
     public static function bvn1Verification(): array;
 
+
     public static function ninImageVerification(): array;
+
 
     public static function ninWithoutFaceVerification(): array;
 
+
     public static function ninWithFaceVerification(): array;
+
 
     public static function tinVerification(): array;
 
+
     public static function basicCACVerification(): array;
+
 
     public static function advanceCACVerification(): array;
 
+
     public static function phoneNumberVerification(): array;
+
 
     public static function verifyBankAccount(): array;
 
+
     public static function getBankCodes(): array;
+
 
     public static function VotersCardImageVerification(): array;
 
+
     public static function votersCardVerification(): array;
+
 
     public static function driverLicenceImageVerification(): array;
 
+
     public static function driverLicenceVerification(): array;
+
 
     public static function getCreditBureauStatement(): array;
 
+
     public static function passportVerification(): array;
+
 
     public static function faceComparison(): array;
 
+
     public static function faceEnrollment(): array;
+
 
     public static function faceLivelinessCheck(): array;
 
+
     public static function faceAuthentication(): array;
+
 
     public static function idFaceMAtching(): array;
 
+
     public static function getWalletBallance(): array;
+
 
     public static function vinIdentificationNumber(): array;
 
+
     public static function interpolBanList(): array;
+
+
 }
+
+
--- src/IdentityPassServiceProvider.php
+++ src/IdentityPassServiceProvider.php
@@ -5,23 +5,19 @@ namespace IdentityPass\IdentityPass;
 use IdentityPass\IdentityPass\Commands\IdentityPassCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-<<<<<<< HEAD
-=======
 use Illuminate\Support\ServiceProvider;
 use IdentityPass\IdentityPass\Transact\Identify;
->>>>>>> e55e055 (api integration)
 
 class IdentityPassServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
+
         $package
             ->name('identity-pass-laravel')
             ->hasConfigFile()
             ->hasCommand(IdentityPassCommand::class);
     }
-<<<<<<< HEAD
-=======
 
     public function packageRegistered(): void
     {
@@ -33,5 +29,4 @@ class IdentityPassServiceProvider extends PackageServiceProvider
 
 
 
->>>>>>> e55e055 (api integration)
 }
